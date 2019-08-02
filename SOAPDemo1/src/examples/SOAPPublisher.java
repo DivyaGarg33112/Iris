6c41db836586072fc3a91ceab8ab2d1a75f0d793
Divya <java.divya.garg@gmail.com>
2019-08-02 12:12:24 +0530

    Commit Done

--- SOAPDemo1/src/examples/SOAPPublisher.java
+++ SOAPDemo1/src/examples/SOAPPublisher.java
@@ -5,7 +5,7 @@ import javax.xml.ws.Endpoint;
 public class SOAPPublisher {
 
 	public static void main(String[] args) {
-		 Endpoint.publish("http://localhost:8889/ws/myperson", new PersonServiceImpl());  
+		 Endpoint.publish("http://localhost:8880/ws/myperson2", new PersonServiceImpl());  
 	}
 
 }
---
+++ SOAPDemo2_Client2/src/main/Test.java
@@ -0,0 +1,13 @@
+package main;
+
+import proxies.ArrayOftLanguage;
+import proxies.CountryInfoService;
+import proxies.CountryInfoServiceSoapType;
+import proxies.TLanguage;
+
+public class Test {
+
+	public static void main(String[] args) {
+			}
+
+}
